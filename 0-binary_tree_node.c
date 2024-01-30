84f3d1bb29fe2b3f114cb84a07dacf9f19fdf3b3
root <omar.elmishtawy@gmail.com>
2024-01-30 11:23:23 -0800

    allocating tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value to add in the node
+ * Return: address of the created node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t* node;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+	{
+		return (NULL);
+	}
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
+	node->parent = parent;
+
+	return (node);
+}
