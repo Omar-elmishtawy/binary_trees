0c1eb42e1f36c1a083f0083261fcc4a933248b87
root <omar.elmishtawy@gmail.com>
2024-01-30 11:48:08 -0800

    fix add left child to a parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,6 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		tmp = parent->left;
+		tmp->parent = node;
 	}
 	parent->left = node;
 	node->left = tmp;
